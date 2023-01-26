b948b81f53b9195649f5be7ba1065d8edfc97ee2
Greyman0 <kutilmusasi@gmail.com>
2023-01-26 23:13:11 +0700

    add simple validation form

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 // bikin jika password tidak sama maka ada alert
 // bikin email tidak sesuai maka ada alert
+
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const pass = document.getElementById("pass"); // password
+const pass_val = document.getElementById("pass_val");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	formCheck();
+});
+
+function formCheck() {
+	if (username.value == "") {
+		username.classList.add("alert");
+	} else if (username.value.length < 6) {
+		username.classList.add("alert");
+	} else {
+		username.classList.remove("alert");
+	}
+
+	if (email.value == "") {
+		email.classList.add("alert");
+	} else if (email.value.includes("@")) {
+		email.classList.add("alert");
+	} else {
+		username.classList.remove("alert");
+	}
+
+	if (pass.value == "") {
+		pass.classList.add("alert");
+	} else if (pass.value.length < 6) {
+		pass.classList.add("alert");
+	} else {
+		pass.classList.remove("alert");
+	}
+
+	if (pass_val.value == "") {
+		pass_val.classList.add("alert");
+	} else if (pass_val.value.length < 6 || pass_val.value !== pass.value) {
+		pass_val.classList.add("alert");
+	} else {
+		pass_val.classList.remove("alert");
+	}
+}
